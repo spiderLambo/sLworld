3ad99ebc3abccb605d0ddd6a30912f04f20db31d
spiderLambo <spiderLambo.dev@outlook.com>
2025-05-11 16:57:30 +0200

    headerhtml

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,14 @@
 <template>
-    afsghj
+    <header>
+        <nav>
+            <ul>
+                <li><img src="../assets/pp.webp" alt="Nom d'utilisateur"></li>
+                <li><img src="../../public/sLmusic-big.svg" alt="🎵 sLmusic"></li>
+                <li>Ajouter</li>
+                <li><img src="../assets/moon-line.svg" alt="Mode nuit"></li>
+            </ul>
+        </nav>
+    </header>
 </template>
 
 <script setup></script>
