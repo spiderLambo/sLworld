b868f1d0dd6ac42b6f498ec8ac91facee784284e
spiderLambo <spiderLambo.dev@outlook.com>
2025-06-01 00:18:59 +0200

    header artiste (sans boutons)

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
   <Header />
-  <Artist />
+  <main>
+    <Artist />
+  </main>
 </template>
 
 <script setup>
 import Header from './components/Header.vue';
 import Artist from './components/Artist.vue';
 </script>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
--- src/components/Artist.vue
+++ src/components/Artist.vue
@@ -1,9 +1,67 @@
 <template>
     <section>
-        <img src="../assets/test/artist.jpeg" alt="PLK">
-        <h6>PLK</h6>
-        <div class="menu"><img src="../assets/" alt=""><img src="" alt=""></div>
+        <header>
+            <img src="../assets/test/artist.jpeg" alt="PLK">
+            <h6>PLK</h6>
+            <div class="menu">
+                <img :src="isDark ? listDark : listLight"
+                    alt="Passer en mode liste" @click="toggleTheme">
+                <img :src="isDark ? gridDark : gridLight"
+                alt="Passer en mode liste" @click="toggleTheme">
+            </div>
+        </header>
     </section>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+import { useTheme } from '../composables/useTheme'
+import listLight from '../assets/list-light.svg'
+import listDark from '../assets/list-dark.svg'
+import gridLight from '../assets/grid-light.svg'
+import gridDark from '../assets/grid-dark.svg'
+
+
+const { isDark, toggleTheme } = useTheme()
+</script>
+
+<style scoped lang="scss">
+$size: 25px;
+$diference: 5px;
+
+section {
+    border: 3px solid #ccc;
+    border-radius: 10px;
+    padding: 1rem;
+    width: 95vw;
+}
+
+header {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 1vw;
+    height: calc($size + 2 * $diference);
+    width: 100%;
+
+    > img {
+        height: $size;
+        border-radius: 50%;
+    }
+
+    h6 {
+        font-size: $size;
+    }
+
+    .menu {
+        margin-left: auto;
+        background: red;
+        display: flex;
+        align-items: center;
+        padding: calc($diference/2);
+
+        img {
+            height: calc($size - $diference);
+        }
+    }
+}
+</style>
