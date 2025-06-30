b14b641fc27e2a8ea6c1d317dfbe81260e8cfcd7
spiderLambo <spiderLambo.dev@outlook.com>
2025-06-30 18:12:00 +0200

    boutton fini

--- src/components/Artist.vue
+++ src/components/Artist.vue
@@ -6,15 +6,19 @@
             
             <div class="menu">
                 <input type="checkbox" id="switch" />
-                <img :src="isDark ? listDark : listLight"
-                alt="Passer en mode liste">
-                <img :src="isDark ? gridDark : gridLight"
-                alt="Passer en mode liste">
-                <label for="switch"></label>
+                <label for="switch">
+                    <img :src="isDark ? listDark : listLight"
+                    alt="Passer en mode liste">
+                    <img :src="isDark ? gridDark : gridLight"
+                    alt="Passer en mode liste">
+
+                </label>
             </div>
         </header>
     </section>
 </template>
+
+
 
 <script setup lang="ts">
 import { useTheme } from '../composables/useTheme'
@@ -27,68 +31,87 @@ import gridDark from '../assets/grid-dark.svg'
 const { isDark, toggleTheme } = useTheme()
 </script>
 
+
+
 <style scoped lang="scss">
 $size: 25px;
 $diference: 5px;
 
 section {
-    border: 3px solid;
-    border-radius: 10px;
-    padding: 1rem;
-    width: 95vw;
+  border: 3px solid;
+  border-radius: 10px;
+  padding: 1rem;
+  width: 95vw;
 }
 
 header {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 1vw;
+  height: calc($size + 2 * $diference);
+  width: 100%;
+
+  > img {
+    height: $size;
+    border-radius: 50%;
+  }
+
+  h6 {
+    font-size: $size;
+  }
+
+  .menu {
+    position: relative;
+    margin-left: auto;
     display: flex;
-    justify-content: flex-start;
     align-items: center;
-    gap: 1vw;
-    height: calc($size + 2 * $diference);
-    width: 100%;
 
-    > img {
-        height: $size;
-        border-radius: 50%;
+    input[type="checkbox"] {
+      height: 0;
+      width: 0;
+      visibility: hidden;
     }
 
-    h6 {
-        font-size: $size;
-    }
+    label {
+      display: flex;
+      align-items: center;
+      gap: $diference;
+      padding: calc($diference / 2);
+      position: relative;
+      border: 1px solid;
+      border-radius: 5px;
+      cursor: pointer;
+      overflow: hidden;
 
-    .menu {
+      &::after {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 50%;
+        height: 100%;
+        background: blue;
+        transition: .1s;
+        z-index: 0;
+      }
+
+      img {
+        z-index: 1;
+        height: calc($size - $diference);
         position: relative;
-        margin-left: auto;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        border-radius: 5px;
-        border: 1px solid ;
-        gap: $diference;
-        padding: calc($diference/2);
-        overflow: hidden;
+        background: none;
+      }
 
-        input[type=checkbox] {
-            height: 0;
-            width: 0;
-            visibility: hidden;
-        }
-
-        img {
-            z-index: 2;
-            background: none;
-            height: $size - $diference;
-        }
-
-        label {
-            position: absolute;
-            z-index: 0;
-            height: 100%; width: 100%;
-            background: red;
-            left: 0;
-        }
-
+      &:active::after {
+        width: 60%;
+      }
     }
 
-    
+    input:checked + label::after {
+      left: 100%;
+      transform: translateX(-100%);
+    }
+  }
 }
 </style>
