5a71ae6c9fd54e441c667d518487346ac7d168e2
spiderLambo <spiderLambo.dev@outlook.com>
2025-07-08 22:20:18 +0200

    footer

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,13 @@
     <Artist />
     <Artist />
   </main>
+  <Footer />
 </template>
 
 <script setup>
 import Header from './components/Header.vue';
 import Artist from './components/Artist.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <style lang="scss" scoped>
---
+++ src/components/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+    <footer>
+        <label for="sort"></label>
+        <select id="sort">
+            <option value="">Trier par</option>
+            <option value="Ajouté recemment">Ajouté recemment</option>
+            <option value="A-Z">A-Z</option>
+            <option value="Z-A">Z-A</option>
+        </select>
+    </footer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 20px;
+
+    label {
+        margin-right: auto;
+        font-weight: bold;
+    }
+
+    select {
+        padding: 5px;
+        border-radius: 4px;
+        cursor: pointer;
+
+        &:focus {
+            outline: none;
+            border-color: #80bdff;
+        }
+    }
+}
+</style>
